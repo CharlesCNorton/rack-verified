ca6c19f35c97bf510dc68238cb7b94578a8b5082
CharlesCNorton <machineelv@gmail.com>
2026-03-19 12:42:34 -0400

    TopoSort.v: topo_sort_valid, index_of helpers, topo_sort_go_ordered

--- theories/TopoSort.v
+++ theories/TopoSort.v
@@ -592,3 +592,295 @@ Proof.
   - exact (nodup_incl_length (map node_id ac.(ac_nodes)) (topo_sort ac)
              Hnd (topo_sort_complete ac Hacyc Hnd)).
 Qed.
+
+(* ================================================================== *)
+(* topo_sort_valid: Kahn's produces a valid topological order          *)
+(* ================================================================== *)
+
+Lemma sb_indeg_zero_no_parent : forall ac remaining id l,
+    sb_in_degree ac remaining id = 0 ->
+    In l ac.(ac_links) ->
+    l.(link_kind) = SupportedBy ->
+    l.(link_to) = id ->
+    mem_string l.(link_from) remaining = false.
+Proof.
+  intros ac remaining id l Hdeg Hlin Hkind Hto.
+  destruct (mem_string l.(link_from) remaining) eqn:E; [| reflexivity].
+  exfalso. unfold sb_in_degree in Hdeg.
+  assert (Hin : In l (filter (fun l0 =>
+    match l0.(link_kind) with
+    | SupportedBy =>
+      String.eqb l0.(link_to) id && mem_string l0.(link_from) remaining
+    | _ => false end) ac.(ac_links))).
+  { apply filter_In. split; [exact Hlin |].
+    rewrite Hkind, Hto, String.eqb_refl, E. reflexivity. }
+  destruct (filter _ _) as [|x xs] eqn:Ef.
+  - destruct Hin.
+  - simpl in Hdeg. discriminate.
+Qed.
+
+Lemma index_of_app_left : forall s l1 l2 i,
+    index_of s l1 = Some i ->
+    index_of s (l1 ++ l2) = Some i.
+Proof.
+  intros s l1. induction l1 as [|a l1' IH]; intros l2 i H; simpl in *.
+  - discriminate.
+  - destruct (String.eqb s a) eqn:E.
+    + exact H.
+    + destruct (index_of s l1') eqn:Ei; [| discriminate].
+      injection H as <-. rewrite (IH l2 n eq_refl). reflexivity.
+Qed.
+
+Lemma index_of_app_right : forall s l1 l2 i,
+    (forall j, index_of s l1 = Some j -> False) ->
+    index_of s l2 = Some i ->
+    index_of s (l1 ++ l2) = Some (length l1 + i).
+Proof.
+  intros s l1. induction l1 as [|a l1' IH]; intros l2 i Hnone H; simpl in *.
+  - exact H.
+  - destruct (String.eqb s a) eqn:E.
+    + exfalso. exact (Hnone 0 eq_refl).
+    + assert (Hnone' : forall j, index_of s l1' = Some j -> False).
+      { intros j Hj. exact (Hnone (S j) ltac:(rewrite Hj; reflexivity)). }
+      rewrite (IH l2 i Hnone' H). reflexivity.
+Qed.
+
+Lemma index_of_Some_In : forall s l i,
+    index_of s l = Some i -> In s l.
+Proof.
+  intros s l. induction l as [|x xs IH]; intros i H; simpl in H.
+  - discriminate.
+  - destruct (String.eqb s x) eqn:Heq.
+    + left. apply String.eqb_eq in Heq. exact (eq_sym Heq).
+    + destruct (index_of s xs) eqn:Hrest; [| discriminate].
+      right. exact (IH n eq_refl).
+Qed.
+
+Lemma index_of_None_not_In : forall s l,
+    (forall i, index_of s l = Some i -> False) -> ~ In s l.
+Proof.
+  intros s l H Hin.
+  induction l as [|a l' IH].
+  - destruct Hin.
+  - simpl in H. destruct Hin as [<- | Hin'].
+    + rewrite String.eqb_refl in H. exact (H 0 eq_refl).
+    + apply IH; [| exact Hin'].
+      intros i Hi. destruct (String.eqb s a) eqn:E.
+      * exact (H 0 eq_refl).
+      * exact (H (S i) ltac:(rewrite Hi; reflexivity)).
+Qed.
+
+Lemma not_In_index_of_None : forall s l,
+    ~ In s l -> forall i, index_of s l = Some i -> False.
+Proof.
+  intros s l Hni i Hi.
+  exact (Hni (index_of_Some_In s l i Hi)).
+Qed.
+
+Lemma index_of_In : forall s l,
+    In s l -> NoDup l -> exists i, index_of s l = Some i.
+Proof.
+  intros s l Hin Hnd. induction l as [|x xs IH].
+  - destruct Hin.
+  - inversion Hnd as [| ? ? Hna Hnd']; subst.
+    simpl. destruct (String.eqb s x) eqn:Heq.
+    + exists 0. reflexivity.
+    + destruct Hin as [<- | Hin].
+      * rewrite String.eqb_refl in Heq. discriminate.
+      * destruct (IH Hin Hnd') as [i Hi].
+        rewrite Hi. exists (S i). reflexivity.
+Qed.
+
+Lemma index_of_lt : forall s l i,
+    index_of s l = Some i -> i < length l.
+Proof.
+  intros s l. induction l as [|x xs IH]; intros i H; simpl in *.
+  - discriminate.
+  - destruct (String.eqb s x) eqn:Heq.
+    + injection H as <-. apply Nat.lt_0_succ.
+    + destruct (index_of s xs) eqn:Hrest; [| discriminate].
+      injection H as <-. apply -> Nat.succ_lt_mono.
+      exact (IH n eq_refl).
+Qed.
+
+Lemma topo_sort_go_ordered :
+  forall ac fuel remaining acc,
+    Acyclic ac ->
+    NoDup remaining -> NoDup acc ->
+    (forall x, In x acc -> ~ In x remaining) ->
+    (forall x, In x (map node_id ac.(ac_nodes)) ->
+      In x acc \/ In x remaining) ->
+    fuel >= length remaining ->
+    (forall x, In x remaining ->
+      In x (map node_id ac.(ac_nodes))) ->
+    (forall l, In l ac.(ac_links) ->
+      l.(link_kind) = SupportedBy ->
+      In l.(link_to) acc ->
+      ~ In l.(link_from) remaining) ->
+    (forall l, In l ac.(ac_links) ->
+      l.(link_kind) = SupportedBy ->
+      In l.(link_from) acc -> In l.(link_to) acc ->
+      exists i j, index_of l.(link_from) acc = Some i /\
+                   index_of l.(link_to) acc = Some j /\ i < j) ->
+    forall l, In l ac.(ac_links) ->
+      l.(link_kind) = SupportedBy ->
+      In l.(link_from) (map node_id ac.(ac_nodes)) ->
+      In l.(link_to) (map node_id ac.(ac_nodes)) ->
+      exists i j,
+        index_of l.(link_from) (topo_sort_go ac fuel remaining acc) = Some i /\
+        index_of l.(link_to) (topo_sort_go ac fuel remaining acc) = Some j /\
+        i < j.
+Proof.
+  intros ac fuel. induction fuel as [|f IH];
+    intros remaining acc Hacyc Hnd_rem Hnd_acc Hdisj Hpart Hfuel
+           Hnodes Hup Hord l Hlin Hkind Hfrom_n Hto_n.
+  - destruct remaining; [| simpl in Hfuel; inversion Hfuel].
+    simpl. apply Hord; [exact Hlin | exact Hkind | |].
+    + destruct (Hpart _ Hfrom_n) as [H|H]; [exact H | destruct H].
+    + destruct (Hpart _ Hto_n) as [H|H]; [exact H | destruct H].
+  - simpl.
+    destruct remaining as [|r0 rest] eqn:Hrem.
+    + apply Hord; [exact Hlin | exact Hkind | |].
+      * destruct (Hpart _ Hfrom_n) as [H|H]; [exact H | destruct H].
+      * destruct (Hpart _ Hto_n) as [H|H]; [exact H | destruct H].
+    + destruct (find (fun id0 => Nat.eqb (sb_in_degree ac (r0 :: rest) id0) 0)
+                  (r0 :: rest)) as [peeled|] eqn:Hfind.
+      * assert (Hpeel_in : In peeled (r0 :: rest))
+          by exact (find_In _ _ _ Hfind).
+        assert (Hpeel_deg : sb_in_degree ac (r0 :: rest) peeled = 0).
+        { apply find_some in Hfind. apply Nat.eqb_eq. exact (proj2 Hfind). }
+        apply IH; clear IH.
+        -- exact Hacyc.
+        -- exact (NoDup_filter _ _ Hnd_rem).
+        -- apply NoDup_app; [exact Hnd_acc | |].
+           ++ constructor; [exact (fun H => H) | constructor].
+           ++ intros x Hx [Heq_px | []].
+              rewrite Heq_px in Hpeel_in.
+              exact (Hdisj x Hx Hpeel_in).
+        -- intros x Hx Hx'.
+           apply in_app_or in Hx. apply filter_remove_In in Hx'.
+           destruct Hx as [Hxa | [Heq_px | []]].
+           ++ exact (Hdisj x Hxa (proj1 Hx')).
+           ++ exact (proj2 Hx' (eq_sym Heq_px)).
+        -- intros x Hxn. destruct (Hpart x Hxn) as [Hxa | Hxr].
+           ++ left. apply in_or_app. left. exact Hxa.
+           ++ destruct (String.eqb x peeled) eqn:Exid.
+              ** left. apply in_or_app. right.
+                 apply String.eqb_eq in Exid. rewrite Exid. left. reflexivity.
+              ** right. apply In_filter_remove; [exact Hxr |].
+                 intro Heq. rewrite Heq in Exid.
+                 rewrite String.eqb_refl in Exid. discriminate.
+        -- assert (Hlt : length (filter (fun x => negb (String.eqb x peeled))
+                            (r0 :: rest)) < length (r0 :: rest))
+             by exact (filter_remove_length peeled (r0 :: rest) Hpeel_in Hnd_rem).
+           apply Nat.lt_succ_r.
+           exact (Nat.lt_le_trans _ _ _ Hlt Hfuel).
+        -- intros x Hx. apply filter_remove_In in Hx.
+           exact (Hnodes x (proj1 Hx)).
+        -- intros l0 Hl0 Hk0 Hto0 Hfr0.
+           apply in_app_or in Hto0.
+           destruct Hto0 as [Hto_acc | [Hto_eq | []]].
+           ++ apply filter_remove_In in Hfr0.
+              exact (Hup l0 Hl0 Hk0 Hto_acc (proj1 Hfr0)).
+           ++ apply filter_remove_In in Hfr0.
+              destruct Hfr0 as [Hfr_rem _].
+              assert (Hmem : mem_string l0.(link_from) (r0 :: rest) = false)
+                by exact (sb_indeg_zero_no_parent ac (r0 :: rest) peeled l0
+                           Hpeel_deg Hl0 Hk0 (eq_sym Hto_eq)).
+              assert (Hin : In l0.(link_from) (r0 :: rest) -> False).
+              { intro Hin. apply In_existsb in Hin.
+                unfold mem_string in Hmem. rewrite Hin in Hmem. discriminate. }
+              exact (Hin Hfr_rem).
+        -- intros l0 Hl0 Hk0 Hfr0 Hto0.
+           apply in_app_or in Hfr0. apply in_app_or in Hto0.
+           destruct Hfr0 as [Hfr_a | [Hfr_eq | []]];
+           destruct Hto0 as [Hto_a | [Hto_eq | []]].
+           ++ destruct (Hord l0 Hl0 Hk0 Hfr_a Hto_a) as [i [j [Hi [Hj Hlt]]]].
+              exists i, j. repeat split.
+              ** exact (index_of_app_left _ _ [peeled] _ Hi).
+              ** exact (index_of_app_left _ _ [peeled] _ Hj).
+              ** exact Hlt.
+           ++ destruct (index_of_In l0.(link_from) acc Hfr_a Hnd_acc) as [i Hi].
+              assert (Hpeel_not_acc : ~ In peeled acc)
+                by exact (fun H => Hdisj peeled H Hpeel_in).
+              exists i, (length acc). repeat split.
+              ** exact (index_of_app_left _ _ [peeled] _ Hi).
+              ** replace (link_to l0) with peeled by exact Hto_eq.
+                 assert (Hidx : index_of peeled [peeled] = Some 0)
+                   by (simpl; rewrite String.eqb_refl; reflexivity).
+                 pose proof (index_of_app_right peeled acc [peeled] 0
+                   (not_In_index_of_None _ _ Hpeel_not_acc) Hidx) as Hres.
+                 rewrite Nat.add_0_r in Hres. exact Hres.
+              ** exact (Nat.lt_le_trans _ _ _ (index_of_lt _ _ _ Hi) (Nat.le_refl _)).
+           ++ exfalso.
+              rewrite Hfr_eq in Hpeel_in.
+              exact (Hup l0 Hl0 Hk0 Hto_a Hpeel_in).
+           ++ exfalso.
+              apply (Hacyc peeled). apply R_Step.
+              unfold supportedby_children. apply in_map_iff.
+              exists l0. split; [exact (eq_sym Hto_eq) |].
+              apply filter_In. split; [exact Hl0 |].
+              rewrite (eq_sym Hfr_eq).
+              simpl. rewrite Hk0. rewrite String.eqb_refl. reflexivity.
+        -- exact Hlin.
+        -- exact Hkind.
+        -- exact Hfrom_n.
+        -- exact Hto_n.
+      * exfalso.
+        destruct (find_zero_indeg ac (r0 :: rest) ltac:(discriminate)
+                    Hnodes Hacyc) as [id' Hfind'].
+        rewrite Hfind in Hfind'. discriminate.
+Qed.
+
+Theorem topo_sort_valid : forall ac,
+    Acyclic ac ->
+    NoDup (map node_id ac.(ac_nodes)) ->
+    no_dangling_links ac ->
+    verify_topo_order ac (topo_sort ac) = true.
+Proof.
+  intros ac Hacyc Hnd Hndl.
+  unfold verify_topo_order.
+  apply Bool.andb_true_iff. split.
+  apply Bool.andb_true_iff. split.
+  - apply forallb_forall. intros l Hlin.
+    destruct l.(link_kind) eqn:Hk; [| reflexivity | reflexivity].
+    assert (Hfrom : In l.(link_from) (map node_id ac.(ac_nodes))).
+    { destruct (Hndl l Hlin) as [[nf Hf] _].
+      apply in_map_iff. exists nf. split.
+      - unfold find_node in Hf. apply find_some in Hf.
+        apply String.eqb_eq. exact (proj2 Hf).
+      - unfold find_node in Hf. apply find_some in Hf. exact (proj1 Hf). }
+    assert (Hto : In l.(link_to) (map node_id ac.(ac_nodes))).
+    { destruct (Hndl l Hlin) as [_ [nt Ht]].
+      apply in_map_iff. exists nt. split.
+      - unfold find_node in Ht. apply find_some in Ht.
+        apply String.eqb_eq. exact (proj2 Ht).
+      - unfold find_node in Ht. apply find_some in Ht. exact (proj1 Ht). }
+    assert (Hfuel : length ac.(ac_nodes) >= length (map node_id ac.(ac_nodes)))
+      by (rewrite length_map; apply Nat.le_refl).
+    destruct (topo_sort_go_ordered ac (length ac.(ac_nodes))
+                (map node_id ac.(ac_nodes)) []
+                Hacyc Hnd (NoDup_nil _)
+                (fun x Hx => match Hx with end)
+                (fun x Hx => or_intror Hx)
+                Hfuel
+                (fun x Hx => Hx)
+                (fun _ _ _ Hto0 => match Hto0 with end)
+                (fun _ _ _ Hfr0 _ => match Hfr0 with end)
+                l Hlin Hk Hfrom Hto)
+      as [i [j [Hi [Hj Hlt]]]].
+    unfold topo_sort. rewrite Hi, Hj. apply Nat.ltb_lt. exact Hlt.
+  - apply forallb_forall. intros n Hn.
+    apply In_existsb.
+    exact (topo_sort_complete ac Hacyc Hnd n.(node_id)
+             (in_map node_id _ n Hn)).
+  - assert (Htsnd := topo_sort_nodup ac Hacyc Hnd).
+    induction (topo_sort ac) as [|a ts IH].
+    + reflexivity.
+    + inversion Htsnd as [| ? ? Hna Hnd']; subst.
+      simpl. apply Bool.andb_true_iff. split.
+      * apply Bool.negb_true_iff.
+        destruct (mem_string a ts) eqn:E; [| reflexivity].
+        exfalso. exact (Hna (existsb_In a ts E)).
+      * exact (IH Hnd').
+Qed.
--- todo.md
+++ todo.md
@@ -3,20 +3,19 @@
 Re-add features from the broken HEAD onto the known-good base (9168543).
 Each item is applied, compiled, and committed individually.
 
-1. TopoSort.v: `topo_sort_valid` — Kahn's produces valid topo order (includes `index_of` helpers, `topo_sort_go_ordered`)
-2. Reflect.v: `prove_well_formed_full` tactic, `prove_evidence_valid_robust` tactic (fix `index_of_Some_In` dependency)
-3. Example.v + CaseStudy.v: replace verbose WF proofs with `prove_well_formed_full`
-4. `agent/rack_agent.py` — Claude CLI wrapper with output sanitizer
-5. `agent/rack_driver.py` — CBMC/Kani/SAW/Alloy evidence driver
-6. `agent/rack_review.py` — PR review bot
-7. `agent/rack_prompt.txt` — RACK domain system prompt
-8. `agent/schema.md` — JSON schema documentation
-9. `.github/workflows/rack-review.yml` — CI: build + zero-Abort gate + review bot
-10. `.github/workflows/build.yml` — add GitHub Pages deployment for coqdoc
-11. `todo.md` — update with remaining open problems
-12. Prove `render_parse_json_string` (needs parse_string_chars fuel monotonicity)
-13. Prove `render_parse_json_roundtrip` for `JArr`/`JObj` (mutual induction)
-14. Prove `avl_insert_find`/`avl_insert_find_other` structurally (~300 lines rotation cases)
-15. Prove `nat_to_string`/`parse_nat_chars` roundtrip (needed for full JSON roundtrip)
-16. ExportUtil.v: `rebuild_claims` preservation proofs (`rebuild_claims_structural_checks` etc.)
-17. Incremental.v: AVL structural correctness (`avl_elements_sorted`, `avl_insert_find_via_elements`, rotation proofs)
+1. Reflect.v: `prove_well_formed_full` tactic, `prove_evidence_valid_robust` tactic (fix `index_of_Some_In` dependency)
+2. Example.v + CaseStudy.v: replace verbose WF proofs with `prove_well_formed_full`
+3. `agent/rack_agent.py` — Claude CLI wrapper with output sanitizer
+4. `agent/rack_driver.py` — CBMC/Kani/SAW/Alloy evidence driver
+5. `agent/rack_review.py` — PR review bot
+6. `agent/rack_prompt.txt` — RACK domain system prompt
+7. `agent/schema.md` — JSON schema documentation
+8. `.github/workflows/rack-review.yml` — CI: build + zero-Abort gate + review bot
+9. `.github/workflows/build.yml` — add GitHub Pages deployment for coqdoc
+10. `todo.md` — update with remaining open problems
+11. Prove `render_parse_json_string` (needs parse_string_chars fuel monotonicity)
+12. Prove `render_parse_json_roundtrip` for `JArr`/`JObj` (mutual induction)
+13. Prove `avl_insert_find`/`avl_insert_find_other` structurally (~300 lines rotation cases)
+14. Prove `nat_to_string`/`parse_nat_chars` roundtrip (needed for full JSON roundtrip)
+15. ExportUtil.v: `rebuild_claims` preservation proofs (`rebuild_claims_structural_checks` etc.)
+16. Incremental.v: AVL structural correctness (`avl_elements_sorted`, `avl_insert_find_via_elements`, rotation proofs)
